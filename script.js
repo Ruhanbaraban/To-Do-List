6f6cf4d223f288c214ee1ac6c356add86a7a7537
Ruhanbaraban <Ruhanbaraban@gmail.com>
2025-11-06 15:45:56 +0300

    commit

--- script.js
+++ script.js
@@ -5,37 +5,92 @@ const addTaskButton = document.getElementById("addTaskButton");
 const showAllTasks = document.getElementById("showAllTasks");
 const showResolvedTasks = document.getElementById("showResolvedTasks");
 const showUnresolvedTasks = document.getElementById("showUnresolvedTasks");
+const boxForActions = document.getElementById("forActions");
 
 const url = "https://jsonplaceholder.typicode.com/todos";
+let timer = null;
 
-fetch(url)
-.then(response => {
-    if(!response.ok){
-        throw new Error(`HTTP error. Status: ${response.status}`);
-    }
-    return response.json()
-})
-.then((response) => {
-    for(let i = 0; i < 5; i++){
-        const task = document.createElement("div");
-        task.className = "task";
-        task.innerHTML = `
-        <div class="leftPartOfTask">
-            <input type="checkbox" class="indicator">
-            <span class="taskValue">${response[i].title}</span>
-        </div>
-        <div class="rightPartOfTask">
-            <img src="" alt="" class="timer">
-            <button class="deleteTask">Удалить задачу</button>
-        </div>
-        `;
-        tasks.appendChild(task);
-    }
-})
-.catch((err) => {
-    alert(err)
-});
+const savedTasks = localStorage.getItem("tasks");
+if(!savedTasks){
+    loadTaskFromServer()
+} else{
+    loadTasksFromLocalStorage()
+}
 
+
+function saveTasksToLocalStorage(){
+    const savedTasks = [];
+
+    tasks.querySelectorAll(".task").forEach((task) => {
+        const indicator = task.querySelector(".indicator");
+        const taskTitleValue = task.querySelector(".taskValue")
+
+        savedTasks.push({
+            taskTitle: taskTitleValue.textContent,
+            taskStatus: indicator.checked
+        });
+    });
+    localStorage.setItem("tasks", JSON.stringify(savedTasks));
+}
+
+function loadTasksFromLocalStorage(){
+    const savedTasks = localStorage.getItem("tasks");
+
+    if(savedTasks){
+        let tasksData = JSON.parse(savedTasks);
+        tasksData.forEach((taskData) => {
+            const task = document.createElement("div");
+            task.className = "task";
+            task.innerHTML = `
+            <div class="leftPartOfTask">
+                <input type="checkbox" class="indicator" ${taskData.taskStatus ? "checked" : ""}>
+                <span class="taskValue">${taskData.taskTitle}</span>
+            </div>
+            <div class="rightPartOfTask">
+                <img src="6ee4c4324c3f1dafd6b7ada58864dcbc.png" alt="" class="timer">
+                <button class="deleteTask">Удалить задачу</button>
+            </div>
+            `;
+            tasks.appendChild(task);
+
+            if(taskData.taskStatus === true){
+                task.classList.add("resolved")
+            }
+        })
+    }
+}
+
+function loadTaskFromServer(){
+    fetch(url)
+        .then(response => {
+            if(!response.ok){
+                throw new Error(`HTTP error. Status: ${response.status}`);
+            }
+            return response.json()
+        })
+        .then((response) => {
+            for(let i = 0; i < 5; i++){
+                const task = document.createElement("div");
+                task.className = "task";
+                task.innerHTML = `
+                <div class="leftPartOfTask">
+                    <input type="checkbox" class="indicator">
+                    <span class="taskValue">${response[i].title}</span>
+                </div>
+                <div class="rightPartOfTask">
+                    <img src="6ee4c4324c3f1dafd6b7ada58864dcbc.png" alt="" class="timer">
+                    <button class="deleteTask">Удалить задачу</button>
+                </div>
+                `;
+                tasks.appendChild(task);
+
+                saveTasksToLocalStorage();
+            }
+        })
+        .catch((err) => {
+            alert(err)
+        });
+}
 
 addTaskButton.addEventListener("click", function addTask(){
     if(taskValue.value.trim().length === 0){
@@ -50,11 +105,15 @@ addTaskButton.addEventListener("click", function addTask(){
         <span class="taskValue">${taskValue.value}</span>
     </div>
     <div class="rightPartOfTask">
-        <img src="" alt="" class="timer">
+        <img src="6ee4c4324c3f1dafd6b7ada58864dcbc.png" alt="" class="timer">
         <button class="deleteTask">Удалить задачу</button>
     </div>
     `;
     tasks.appendChild(task);
+
+    taskValue.value = "";
+     
+    saveTasksToLocalStorage();
 });
 
 tasks.addEventListener("change", (e) => {
@@ -66,13 +125,16 @@ tasks.addEventListener("change", (e) => {
         } else(
             task.classList.remove("resolved")
         )
+        saveTasksToLocalStorage()
     }
 })
 
 tasks.addEventListener("click", (e) => {
     if(e.target.classList.contains("deleteTask")){
         const task = e.target.closest(".task");
+        clearTimeout(timer)
         tasks.removeChild(task);
+        saveTasksToLocalStorage()
     }
 })
 
@@ -103,4 +165,38 @@ showUnresolvedTasks.addEventListener("click", () => {
             task.classList.remove("filtered")
         }
     })
+})
+
+tasks.addEventListener("click", (e) => {
+    if(e.target.classList.contains("timer")){
+
+        boxForActions.innerHTML = `
+            <input type="number" placeholder="В минутах" id="timeForTimer">
+            <button id="submitTimer">Установить таймер</button>
+            <span id="closeBoxBtn">X</span>
+        `
+
+        const timerInput = boxForActions.querySelector("#timeForTimer");
+        const submitTimer = boxForActions.querySelector("#submitTimer");
+        const closeBoxBtn = boxForActions.querySelector("#closeBoxBtn");
+
+        closeBoxBtn.addEventListener("click", () => {
+            boxForActions.innerHTML = "";
+        })
+
+        submitTimer.addEventListener("click", () => {
+            const minutes = parseInt(timerInput.value);
+            clearTimeout(timer)
+            if(minutes <= 0){
+                alert("Количество минут должно быть положительным!");
+                return;
+            }
+            timer = setTimeout(() => {
+                alert("Время вышло!");
+                timer = null;
+            }, minutes * 60000);
+            timerInput.value = "";
+        })
+    }
+
 })
